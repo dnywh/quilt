1c64203c94be574c1d36e12f153b6bfcc9548c81
Danny White <3104761+dnywh@users.noreply.github.com>
2025-12-21 16:18:42 +1000

    Squashed commit of the following:
    commit c1d01c2979fa70b824edb4182257df8583416ae9
    Author: Danny White <3104761+dnywh@users.noreply.github.com>
    Date:   Sun Dec 21 16:18:28 2025 +1000
    
        extra map
    
    commit 36b772f961818d155e34ec7acc9de7242a382c2d
    Author: Danny White <3104761+dnywh@users.noreply.github.com>
    Date:   Sat Dec 20 16:10:51 2025 +1000
    
        static map only
    
    commit 208a7db307d3ef0b97f4c915378b1f9cffb2c946
    Author: Danny White <3104761+dnywh@users.noreply.github.com>
    Date:   Sat Dec 20 16:00:20 2025 +1000
    
        map viewer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      PUBLIC_MAPTILER_API_KEY: envField.string({
+        context: 'client',
+        access: 'public',
+      }),
+    },
+  },
+});
